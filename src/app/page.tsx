2ae79536a7d5fa9d912fbf54cde7a75ee9a2ec73
Asterixter <114302678+Harishvjk@users.noreply.github.com>
2025-10-31 14:50:21 +0530

    Customized Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home() {
   return (
     <div>
-      <p>Za Warudooo</p>
+      <div className="p-2 flex-row space-y-4">
+        <Button variant={"elevated"}>Hello</Button>
+        <Input placeholder="Enter some text"></Input>
+        <Progress value={50}></Progress>
+        <Textarea placeholder="Enter anything"></Textarea>
+        <Checkbox></Checkbox>
+      </div>
     </div>
   );
 }
